51068d14b6d8e3b6028ef8a582742005a5b91a23
Tomas Baltrushaitis <tbaltrushaitis@users.noreply.github.com>
2018-02-22 00:52:42 +0200

    Reduced empty lines count

--- index.php
+++ index.php
@@ -1,32 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <title>Game</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Calc">
+<head>
+  <meta charset="utf-8">
+  <title>Snake Game</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="Snake Game">
+  <link href="style.css" rel="stylesheet">
+</head>
 
-
-    <link href="style.css" rel="stylesheet">
-
-
-  </head>
-
-  <body>
-	<div class="main-block-nav">
-		<div class="score">0</div>
-		<div class="new-game">NEW GAME</div>
-	</div>
-	<div class="main-block">
-
-			
-
-		
-	</div>
-  
+<body>
+  <div class="main-block-nav">
+    <div class="score">0</div>
+    <div class="new-game">NEW GAME</div>
+  </div>
+  <div class="main-block">
+  </div>
   
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
   <script src="./game.js"></script>
-  
-  </body>
+</body>
 </html>
